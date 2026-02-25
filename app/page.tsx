e5c70542b89067301319108b8920361a941aa8fb
John-Thailand <naoki.honda@mirrorfit.jp>
2026-02-25 22:56:19 +0900

    getAllLessonsを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Link from "next/link";
 import { createClient } from "./lib/supabase/server";
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const supabase = await createClient();
+
+const getAllLessons = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*");
+  return lessons;
+};
 
 export default async function Home() {
-  const supabase = await createClient();
-
-  const { data: lessons } = await supabase.from("lesson").select("*");
+  const lessons = await getAllLessons();
 
   return (
     <main className="w-full max-w-3xl mx-auto my-16 px-2">
