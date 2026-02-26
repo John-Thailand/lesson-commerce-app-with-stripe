560c46e1f7cc51261f143430804cb2fccb3c2676
John-Thailand <naoki.honda@mirrorfit.jp>
2026-02-26 22:41:40 +0900

    詳細レッスンデータをブラウザへ出力する

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,17 +1,14 @@
-import React from "react";
 import { createClient } from "../lib/supabase/server";
 
-const supabase = await createClient();
-
-const getDetailLesson = async (id: number) => {
+const LessonDetailPage = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const supabase = await createClient();
+  const { id: idStr } = await params;
+  const id = Number(idStr);
   const { data: lesson } = await supabase.from("lesson").select("*").eq("id", id).single();
-  return lesson;
-};
-
-const LessonDetailPage = async ({ params }: { params: { id: number } }) => {
-  const lesson = await getDetailLesson(params.id)
-  console.log(lesson)
-  return <div>LessonDetailPage</div>
+  return <div className="w-full max-w-3xl mx-auto py-16 px-8">
+    <h1 className="text-3xl mb-6">{lesson?.title}</h1>
+    <p className="mb-8">{lesson?.description}</p>
+  </div>
 };
 
 export default LessonDetailPage;
