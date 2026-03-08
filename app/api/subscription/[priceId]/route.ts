2f7d52c78832417ad574d6cf2063490678ca15d7
John-Thailand <naoki.honda@mirrorfit.jp>
2026-03-08 14:03:10 +0900

    カードチェックアウトのセッションを作成してそのIDを返す

--- app/api/subscription/[priceId]/route.ts
+++ app/api/subscription/[priceId]/route.ts
@@ -1,6 +1,7 @@
 import { createServerClient } from "@supabase/ssr/dist/main/createServerClient";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import initStripe from "stripe";
 
 export async function GET(
   req: NextRequest,
@@ -26,14 +27,27 @@ export async function GET(
   const { data } = await supabase.auth.getUser();
   const user = data.user;
 
+  if (!user) {
+    return NextResponse.json("Unauthorized", { status: 401 });
+  }
+
   const { data: stripe_customer_data } = await supabase
     .from("profile")
     .select("stripe_customer")
     .eq("id", user?.id)
     .single();
 
-  return NextResponse.json({
-    ...user,
-    stripe_customer_data,
-  })
+  const priceId = params.priceId;
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+
+  const session = await stripe.checkout.sessions.create({
+    customer: stripe_customer_data?.stripe_customer,
+    mode: "subscription",
+    payment_method_types: ["card"],
+    line_items: [{ price: priceId, quantity: 1 }],
+    success_url: "http://localhost:3000/payment/success",
+    cancel_url: "http://localhost:3000/payment/cancelled",
+  });
+
+  return NextResponse.json({ id: session.id });
 }
