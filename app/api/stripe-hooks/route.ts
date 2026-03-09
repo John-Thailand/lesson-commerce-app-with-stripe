753f0a531379e45d6d18d9facd4d8ccf6b87eab6
John-Thailand <naoki.honda@mirrorfit.jp>
2026-03-09 20:33:12 +0900

    Stripe Webhooksでイベントのタイプ毎に処理を分岐させる

--- app/api/stripe-hooks/route.ts
+++ app/api/stripe-hooks/route.ts
@@ -1,5 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
+import initStripe from "stripe";
 
 export async function POST(req: NextRequest) {
+  const stripe = new initStripe(process.env.STRIPE_SECRET_KEY!);
+  const endpointSecret = process.env.STRIPE_SIGNING_SECRET;
+  const signature = req.headers.get("stripe-signature");
+
+  const reqBuffer = Buffer.from(await req.arrayBuffer());
+
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(reqBuffer, signature!, endpointSecret!);
+    switch (event.type) {
+      case "customer.subscription.created":
+        break;
+      case "customer.subscription.deleted":
+        break;
+      case "customer.subscription.updated":
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+  } catch (err: any) {
+    return NextResponse.json(`Webhook Error: ${err.message}`, { status: 401 });
+  }
+
   return NextResponse.json({ received: true })
 };
