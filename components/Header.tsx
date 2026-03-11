098d9e7f4b43e79c0c72be11205c6903dbe7b112
John-Thailand <naoki.honda@mirrorfit.jp>
2026-03-11 18:49:13 +0900

    ヘッダーにダッシュボードを追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import { Button } from "./ui/button"
 import AuthClientButton from "./auth/AuthClientButton";
+import { createClient } from "@/app/lib/supabase/client";
 
-const Header = () => {
+const Header = async () => {
+  const supabase = await createClient();
+  const { data: user } = await supabase.auth.getSession();
+
   return (
     <div className="flex py-4 px-6 border-b border-gray-200">
       <Link href={"/"}>
         <Button variant={"outline"}>ホーム</Button>
       </Link>
+      {user.session && (
+        <Link href={"/dashboard"} className="ml-4">
+          <Button variant={"outline"}>ダッシュボード</Button>
+        </Link>
+      )}
       <Link href={"/pricing"} className="ml-4">
         <Button variant={"outline"}>価格</Button>
       </Link>
